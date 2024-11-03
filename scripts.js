707e6853979474801e24e97dc44376edd94e2175
MarcoGoll <166357601+MarcoGoll@users.noreply.github.com>
2024-11-03 20:31:01 +0100

    create: calender

---
+++ scripts.js
@@ -0,0 +1,38 @@
+const date = new Date();
+let alreadyOpened = [];
+
+function loadCalender() {
+    let loadedOpenDoors = loadFromLocalStorage();
+    if (loadedOpenDoors != null) {
+        alreadyOpened = loadedOpenDoors;
+    }
+    for (let i = 0; i < alreadyOpened.length; i++) {
+        setBackgroundImg(alreadyOpened[i]);
+    }
+}
+
+function openDoor(i) {
+    let day = date.getDate();
+    let month = date.getMonth() + 1;
+
+    if (month == 12) {
+        if (i <= day) {
+            alreadyOpened.push(i);
+            setBackgroundImg(i);
+            saveToLocalStorage()
+        }
+    }
+}
+
+function setBackgroundImg(i) {
+    let door = document.getElementById(`calendarDoor${i}`);
+    door.style.backgroundImage = `url(./img/${i}.jpg)`;
+}
+
+function saveToLocalStorage() {
+    localStorage.setItem("alreadyOpened", JSON.stringify(alreadyOpened));
+}
+
+function loadFromLocalStorage() {
+    return JSON.parse(localStorage.getItem("alreadyOpened"));
+}
